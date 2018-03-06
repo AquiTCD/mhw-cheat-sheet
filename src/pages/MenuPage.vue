4a9a3b55fc2d1b559cc97fb98cc46e58cf721fe9
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-03-06 14:45:34 +0900

    feat(Menu): Add social share buttons

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -2,7 +2,7 @@
 v-ons-page
   v-ons-toolbar(modifier="transparent")
   .header
-    img(src="../assets/icon.png" alt="icon")
+    img(src="@/assets/icon.png" alt="icon")
 
   v-ons-list-title Menu
   v-ons-list
@@ -16,6 +16,19 @@ v-ons-page
       .left
         v-ons-icon(fixed-width :icon="item.icon")
       .center {{ item.label }}
+  .social
+    .fb(data-href="https://mhw-cs.solunita.net" data-mobile-iframe="true")
+      a(a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fmhw-cs.solunita.net%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore")
+        img.shareIcon(src="@/assets/facebook.svg" alt="facebook_icon")
+    .twitter
+      a(href="http://twitter.com/share?url=https://mhw-cs.solunita.net.com")
+        img.shareIcon(src="@/assets/twitter.svg" alt="twitter_icon")
+    .line
+      a(href="http://line.me/R/msg/text/?https://mhw-cs.solunita.net.com")
+        img.shareIcon(src="@/assets/line.svg" alt="line_icon")
+    .hateb
+      a(href="http://b.hatena.ne.jp/add?url=https://mhw-cs.solunita.net.com")
+        img.shareIcon(src="@/assets/hatenabookmark.svg" alt="hatenabookmark_icon")
 </template>
 
 <script>
@@ -71,21 +84,26 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.header {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-img {
-  max-width: 150px;
-}
-
-ons-list-title {
-  text-transform: none;
-}
-
-ons-list-item {
-  cursor: pointer;
-}
+<style lang="stylus" scoped>
+.header
+  text-align: center
+  margin-bottom: 20px
+img
+  max-width: 150px
+ons-list-title
+  text-transform: none
+ons-list-item
+  cursor: pointer
+.social
+  display: flex
+  justify-content: center
+  align-items: center
+.fb, .twitter, .line, .hateb
+  background: #e0e0e0
+  border: 2px solid #000000
+  border-radius: 4px
+  padding: 3px
+  margin: 3px
+.shareIcon
+  width: 18px
 </style>
