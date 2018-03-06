6ee6eff9ee4407129285cc75ec4abb8e8c80b3df
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-03-06 22:14:28 +0900

    docs(about): Update for 0.2.1

--- src/pages/aboutPage.vue
+++ src/pages/aboutPage.vue
@@ -2,6 +2,13 @@
 .about
   h2.header About
   v-ons-card
+    .date 2018-03-06
+    .title 0.2.1
+    ul
+      li メニューにSNS用シェアボタンを追加
+      li 問題報告を埋め込みに変更
+      li iOSのホーム画面用のアイコンを修正
+  v-ons-card
     .date 2018-03-05
     .title 0.2.0
     ul
